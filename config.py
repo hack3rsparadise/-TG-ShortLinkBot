2a568fa07913ce2c962e404c2466d64912cc844b
hack3rsparadise <89106125+hack3rsparadise@users.noreply.github.com>
2021-10-04 20:19:11 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+API_ID = int(environ.get('3552370'))
+API_HASH = environ.get('a52e91be482d5420fcd2147ee42c5482')
+BOT_TOKEN = environ.get('2065474159:AAHIF_N9x8qACzxcsCSjjCF7Qn03nlO6S0I')
+BITLY_KEY = environ.get('ec401c380861422b7fd8016e915180eaacbb7203')
+OWNER = environ.get('OWNER','Latest_OTT')
